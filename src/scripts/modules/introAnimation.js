a91542bbcbf1af6b9cd0ce2240c1eb9ccd2075e6
Thomas Nägele <tom@naegele.it>
2019-10-15 16:44:15 +0200

    - get scrollmagic to work

--- src/scripts/modules/introAnimation.js
+++ src/scripts/modules/introAnimation.js
@@ -1,16 +1,38 @@
-import { TweenLite, TimelineLite } from "gsap"
+
+import 'imports-loader?define=>false!animation.gsap';
+import ScrollMagic from 'ScrollMagic';
 
 class IntroAnimation {
   constructor() {
 
-    this.tlIntro = new TimelineLite();
+    // Fetch all DOM elements
+    this.section = document.querySelector('.intro')
+    this.heart = document.querySelector('#logo__heart')
+    this.mountainBack = document.querySelector('#logo__mountain--back')
+    this.mountainCover = document.querySelector('#logo__mountain--cover')
+    this.mountainFront = document.querySelector('#logo__mountain--front')
 
-    this.logoHeart = document.querySelector('#logo__heart')
-    this.logoMountainBack = document.querySelector('#logo__mountain--back')
-    this.logoMountainCover = document.querySelector('#logo__mountain--cover')
-    this.logoMountainFront = document.querySelector('#logo__mountain--front')
+    // ScrollMagic controller
+    this.ctrl = new ScrollMagic.Controller()
 
-    console.log(this.logoMountainBack)
+    // GSAP timeline
+    this.tlIntro = new TimelineMax()
+
+    this.tlIntro
+      .set(this.heart, { opacity: 0 })
+      .to(this.heart, 1, { opacity: 1 })
+
+    // ScrollMagic scene
+    this.scene = new ScrollMagic.Scene({
+      triggerElement: this.section,
+      triggerHook: "onLeave",
+      duration: "100%"
+    })
+    .setTween(this.tlIntro)
+    .setPin(this.section)
+    .addTo(this.ctrl)
+
+    console.log(this.ctrl)
   }
 }
 
