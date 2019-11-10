51d9e8f746fd379c1f6c7beafbcc4fecbdd86d8c
Thomas Nägele <tom@naegele.it>
2019-11-10 12:57:51 +0100

    - first implementation of animation

--- src/scripts/modules/introAnimation.js
+++ src/scripts/modules/introAnimation.js
@@ -7,6 +7,7 @@ class IntroAnimation {
 
     // Fetch all DOM elements
     this.section = document.querySelector('.intro')
+    this.logo = document.querySelector('#js-logo')
     this.heart = document.querySelector('#logo__heart')
     this.mountainBack = document.querySelector('#logo__mountain--back')
     this.mountainCover = document.querySelector('#logo__mountain--cover')
@@ -16,23 +17,34 @@ class IntroAnimation {
     this.ctrl = new ScrollMagic.Controller()
 
     // GSAP timeline
-    this.tlIntro = new TimelineMax()
+    this.tlLogo = new TimelineMax()
 
-    this.tlIntro
+    this.tlLogo
+      .set(this.logo, { x: "20%", y:"-78%", scale: 1.5 })
       .set(this.heart, { opacity: 0 })
-      .to(this.heart, 1, { opacity: 1 })
+      .set(this.mountainBack, { fill: '#50524F', stroke: "transparent" })
+      .set(this.mountainCover, { opacity: 0 })
+      .set(this.mountainFront, {
+        fill: '#D7BCAE',
+        stroke: "transparent",
+        x: "40%",
+        scale: 2,
+        transformOrigin: "center bottom" })
+      .to(this.logo, 1, { x: "0%", y: "0%", scale: 1 }, "first")
+      .to(this.mountainFront, 1, { x: "0%", scale: 1 }, "first")
+      .to(this.mountainBack, 1, { fill: 'transparent', stroke: "#C2D6C8" }, "first")
+      .to(this.mountainCover, 1, { opacity: 1 }, "first")
+      .to(this.mountainFront, 1, { fill: 'transparent', stroke: "#89A491" }, "first")
+      .to(this.heart, 1, { opacity: 1 }, "second")
 
     // ScrollMagic scene
     this.scene = new ScrollMagic.Scene({
-      triggerElement: this.section,
-      triggerHook: "onLeave",
-      duration: "100%"
+      triggerElement: document.querySelector('body'),
+      triggerHook: 0,
+      duration: "80%"
     })
-    .setTween(this.tlIntro)
-    .setPin(this.section)
+    .setTween(this.tlLogo)
     .addTo(this.ctrl)
-
-    console.log(this.ctrl)
   }
 }
 
