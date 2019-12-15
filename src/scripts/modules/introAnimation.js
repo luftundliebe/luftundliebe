430f4ff761ab0e053d74543d506a7984b07595fa
Thomas Nägele <tom@naegele.it>
2019-12-15 23:43:14 +0100

    - style intro with css and svg

--- src/scripts/modules/introAnimation.js
+++ src/scripts/modules/introAnimation.js
@@ -5,50 +5,69 @@ import ScrollMagic from 'ScrollMagic';
 class IntroAnimation {
   constructor() {
 
+    this.heart = document.querySelector('#herz')
+    this.mountainBack = document.querySelector('#berg--hinten')
+    this.mountainCover = document.querySelector('#berg--abdeckung')
+    this.mountainFront = document.querySelector('#berg--vorne')
+    this.claim = document.querySelector('#claim')
+
+    this.tlIntro = new TimelineMax()
+
+    this.tlIntro
+      .to(this.mountainBack, 1, { scale: 1, y: 0 }, '0')
+      .to(this.mountainFront, 1, { transform: 'translateY(0) scale(1)'}, '0')
     // Logo letters animation
-    this.chars = document.querySelectorAll('.char')
-    this.logo = document.querySelector('#js-logo')
-    this.intro = document.querySelector('#js-intro')
-
-    this.tlCharsIntro = new TimelineMax()
-    this.tlCharsIntro.staggerTo(this.chars, 1, { opacity: 1, scale: 1, ease: Power1.easeOut }, .1)
-
-    this.tlCharsOutro = new TimelineMax()
-    this.tlCharsOutro
-        // .staggerTo(this.chars, 1, { opacity: 0 }, .1, '0')
-        // .staggerTo(this.chars, 2, { y: '-200%', ease: Power1.easeOut }, .1, '0')
-        .to(this.logo, 10, { y: '80vh', color: '#343633'}, '0')
-
-    // Mountains animation
-    this.mountainLeft = document.querySelector('#js-mountain--left')
-    this.mountainRight = document.querySelector('#js-mountain--right')
-
-    this.tlMountains = new TimelineMax()
-    this.tlMountains
-        .to(this.mountainLeft, 3, { x: '-300vw', bottom: '-100vh', opacity: 0 }, '0')
-        .to(this.mountainRight, 3, { x: '150vw', scale: 2, bottom: '-100vh', transformOrigin:'center bottom', opacity: 0 }, '0')
+    // this.chars = document.querySelectorAll('.char')
+    // this.logo = document.querySelector('#js-logo')
+    // this.intro = document.querySelector('#js-intro')
+    //
+    // this.tlCharsIntro = new TimelineMax()
+    // this.tlCharsIntro.staggerTo(this.chars, 1, { opacity: 1, scale: 1, ease: Power1.easeOut }, .1)
+    //
+    // this.tlCharsOutro = new TimelineMax()
+    // this.tlCharsOutro
+    //     // .staggerTo(this.chars, 1, { opacity: 0 }, .1, '0')
+    //     // .staggerTo(this.chars, 2, { y: '-200%', ease: Power1.easeOut }, .1, '0')
+    //     .to(this.logo, 10, { y: '80vh', color: '#343633'}, '0')
+    //
+    // // Mountains animation
+    // this.mountainLeft = document.querySelector('#js-mountain--left')
+    // this.mountainRight = document.querySelector('#js-mountain--right')
+    //
+    // this.tlMountains = new TimelineMax()
+    // this.tlMountains
+    //     .to(this.mountainLeft, 3, { x: '-300vw', bottom: '-100vh', opacity: 0 }, '0')
+    //     .to(this.mountainRight, 3, { x: '150vw', scale: 2, bottom: '-100vh', transformOrigin:'center bottom', opacity: 0 }, '0')
 
     // ScrollMagic controller
     this.ctrl = new ScrollMagic.Controller()
 
-    // Logo scene
-    this.scene = new ScrollMagic.Scene({
+    this.IntroScene = new ScrollMagic.Scene({
       triggerElement: document.querySelector('#js-intro'),
       triggerHook: 0,
-      duration: "200%"
+      duration: '100%'
     })
-    .setTween(this.tlCharsOutro)
+    .setTween(this.tlIntro)
     .addTo(this.ctrl)
 
-    // Mountain scene
-    this.sceneMountains = new ScrollMagic.Scene({
-      triggerElement: document.querySelector('#js-intro'),
-      triggerHook: 0,
-      duration: "50%"
-    })
-    .setTween(this.tlMountains)
-    .setPin(this.intro)
-    .addTo(this.ctrl)
+    // // Logo scene
+    // this.scene = new ScrollMagic.Scene({
+    //   triggerElement: document.querySelector('#js-intro'),
+    //   triggerHook: 0,
+    //   duration: "200%"
+    // })
+    // .setTween(this.tlCharsOutro)
+    // .addTo(this.ctrl)
+    //
+    // // Mountain scene
+    // this.sceneMountains = new ScrollMagic.Scene({
+    //   triggerElement: document.querySelector('#js-intro'),
+    //   triggerHook: 0,
+    //   duration: "50%"
+    // })
+    // .setTween(this.tlMountains)
+    // .setPin(this.intro)
+    // .addTo(this.ctrl)
 
     // Fetch all DOM elements
     // this.section = document.querySelector('.intro')
