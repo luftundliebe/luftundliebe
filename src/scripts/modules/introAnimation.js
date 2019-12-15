e8406c7fea0a09683876378819c3ccafb60a4f0e
Thomas Nägele <tom@naegele.it>
2019-12-15 22:53:26 +0100

    - trying intro animation

--- src/scripts/modules/introAnimation.js
+++ src/scripts/modules/introAnimation.js
@@ -5,26 +5,49 @@ import ScrollMagic from 'ScrollMagic';
 class IntroAnimation {
   constructor() {
 
+    // Logo letters animation
     this.chars = document.querySelectorAll('.char')
+    this.logo = document.querySelector('#js-logo')
+    this.intro = document.querySelector('#js-intro')
 
     this.tlCharsIntro = new TimelineMax()
     this.tlCharsIntro.staggerTo(this.chars, 1, { opacity: 1, scale: 1, ease: Power1.easeOut }, .1)
 
     this.tlCharsOutro = new TimelineMax()
     this.tlCharsOutro
-        .staggerTo(this.chars, 1, { opacity: 0 }, .1, '0')
-        .staggerTo(this.chars, 2, { y: '-200%', ease: Power1.easeOut }, .1, '0')
+        // .staggerTo(this.chars, 1, { opacity: 0 }, .1, '0')
+        // .staggerTo(this.chars, 2, { y: '-200%', ease: Power1.easeOut }, .1, '0')
+        .to(this.logo, 10, { y: '80vh', color: '#343633'}, '0')
+
+    // Mountains animation
+    this.mountainLeft = document.querySelector('#js-mountain--left')
+    this.mountainRight = document.querySelector('#js-mountain--right')
+
+    this.tlMountains = new TimelineMax()
+    this.tlMountains
+        .to(this.mountainLeft, 3, { x: '-300vw', bottom: '-100vh', opacity: 0 }, '0')
+        .to(this.mountainRight, 3, { x: '150vw', scale: 2, bottom: '-100vh', transformOrigin:'center bottom', opacity: 0 }, '0')
 
     // ScrollMagic controller
     this.ctrl = new ScrollMagic.Controller()
 
-    // ScrollMagic scene
+    // Logo scene
     this.scene = new ScrollMagic.Scene({
-      triggerElement: document.querySelector('body'),
+      triggerElement: document.querySelector('#js-intro'),
       triggerHook: 0,
-      duration: "40%"
+      duration: "200%"
     })
     .setTween(this.tlCharsOutro)
+    .addTo(this.ctrl)
+
+    // Mountain scene
+    this.sceneMountains = new ScrollMagic.Scene({
+      triggerElement: document.querySelector('#js-intro'),
+      triggerHook: 0,
+      duration: "50%"
+    })
+    .setTween(this.tlMountains)
+    .setPin(this.intro)
     .addTo(this.ctrl)
 
     // Fetch all DOM elements
